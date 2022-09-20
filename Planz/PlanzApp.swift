d7a9b5b11626eedc037a37fe9be2cb812277611b
junyng <pom0894@naver.com>
2022-09-20 19:49:30 +0900

    Add starter project

---
+++ Planz/PlanzApp.swift
@@ -0,0 +1,18 @@
+//
+//  PlanzApp.swift
+//  Planz
+//
+//  Created by junyng on 2022/09/20.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PlanzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
