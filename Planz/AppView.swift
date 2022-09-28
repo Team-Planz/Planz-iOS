3f86feadb2080ab2b7a002a838cf1aeb7d432212
Junyeong cho <pom0894@naver.com>
2022-09-28 19:14:56 +0900

    Add starter project
    * Add starter project
    
    * Add .gitignore
    
    * Add CI workflow
    
    * Fix build simulator os version
    
    * Apply discord notification after build
    
    * Update ci workflow
    
    - Rename workflow file
    - Remove unused step
    
    * Update build and test workflow
    
    - Apply test failure notification
    - Add workflow status badge

---
+++ Planz/AppView.swift
@@ -0,0 +1,21 @@
+//
+//  AppView.swift
+//  Planz
+//
+//  Created by junyng on 2022/09/20.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Planz-iOS
+[![build-and-test](https://github.com/Team-Planz/Planz-iOS/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/Team-Planz/Planz-iOS/actions/workflows/build-and-test.yml)
+
