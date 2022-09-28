6e47b77bc45b8f93d0fe1c84296637261e060338
Junyeong cho <pom0894@naver.com>
2022-09-28 19:14:56 +0900

    Add starter project
    * Add starter project
    
    * Add .gitignore
    
    * Add CI workflow
    
    * Fix build simulator os version
    
    * Apply discord notification after build
    
    * Update ci workflow
    
    - Rename workflow file
    - Remove unused step
    
    * Update build and test workflow
    
    - Apply test failure notification
    - Add workflow status badge

---
+++ PlanzUITests/PlanzUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PlanzUITestsLaunchTests.swift
+//  PlanzUITests
+//
+//  Created by junyng on 2022/09/20.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import XCTest
+
+final class PlanzUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
