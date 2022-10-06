7b30f4b261e64bdc8383c32b48fd6295a0e9fe08
junyng <pom0894@naver.com>
2022-10-06 21:00:06 +0900

    Update package dependencies
    - Add `ComposableArchitecture` module
    - Add `Introspect` module
    - Add `Then` module

---
+++ AppPackage/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppPackage",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "AppPackage",
+            targets: ["AppPackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.40.2"),
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.4"),
+        .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(
+            name: "AppPackage",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "Introspect", package: "SwiftUI-Introspect"),
+                .product(name: "Then", package: "Then")
+            ])
+    ]
+)
