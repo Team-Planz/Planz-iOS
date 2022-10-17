9c3538963c7cd0079d87b9166795c548b7a0a042
Junyeong cho <pom0894@naver.com>
2022-10-17 13:57:22 +0900

    GROW-321 Add TimeTableView (#5)
    * GROW-321 Add timetable view
    
    * GROW-321 Add `TimeTableFeature` module
    
    * GROW-321 Fix gradient layer position
    
    * GROW-321 Add dotted line to timeCell

---
+++ AppPackage/Sources/TimeTableFeature/Lines.swift
@@ -0,0 +1,27 @@
+//
+//  Lines.swift
+//  Planz
+//
+//  Created by junyng on 2022/10/15.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import SwiftUI
+
+struct HorizontalLine: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        return path
+    }
+}
+
+struct VerticalLine: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        return path
+    }
+}
