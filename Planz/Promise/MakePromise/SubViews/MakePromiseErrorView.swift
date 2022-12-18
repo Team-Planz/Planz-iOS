a4fbe9d5efe25469ba72a14b58137c4f6f5b79dc
Andrew <escapeanaemia@gmail.com>
2022-12-18 20:53:06 +0900

    [GROW-262] add error view

---
+++ Planz/Promise/MakePromise/SubViews/MakePromiseErrorView.swift
@@ -0,0 +1,39 @@
+//
+//  MakePromiseErrorView.swift
+//  Planz
+//
+//  Created by 한상준 on 2022/12/18.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+public struct MakePromiseErrorView: View {
+    public var body: some View {
+        VStack {
+            
+            Image("Illustration1")
+            Spacer().frame(height: 12)
+            Text("문제가 발생했습니다.\n다시 시도해주세요.")
+            Spacer().frame(height: 14)
+            RetryButton()
+        }
+    }
+}
+
+public struct RetryButton: View {
+    let cornerRadius: CGFloat = 7
+    public var body: some View {
+        Button("다시 시도") {
+            print("Button clicked")
+        }
+        .foregroundColor(Purple._900.scale)
+        .frame(width:96, height: 34)
+        .cornerRadius(cornerRadius)
+        .overlay(
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .stroke(Purple._900.scale, lineWidth: 1)
+        )
+    }
+}
