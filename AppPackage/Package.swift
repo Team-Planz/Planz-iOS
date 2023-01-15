0e1286308f167e029ce49497005db3abf7fa93b4
junyng <pom0894@naver.com>
2023-01-15 14:56:03 +0900

    [GROW-352] Update minimum app target version

--- AppPackage/Package.swift
+++ AppPackage/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppPackage",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "AppPackage",
