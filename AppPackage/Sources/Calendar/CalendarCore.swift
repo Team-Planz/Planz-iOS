54e4a585ed195972e4fded98daa58e81ea13e57b
GangWoon <gangwoon2018@gmail.com>
2023-01-29 14:35:00 +0900

    [GROW-355] Create Calendar View

--- AppPackage/Sources/Calendar/CalendarCore.swift
+++ AppPackage/Sources/Calendar/CalendarCore.swift
@@ -3,11 +3,22 @@ import Foundation
 
 public struct CalendarCore: ReducerProtocol {
     public struct State: Equatable {
+        var monthStateList: IdentifiedArrayOf<MonthState> = []
+        var selectedMonth: Date = .currentMonth
         
+        public init() {
+            
+        }
     }
     
     public enum Action: Equatable {
         case onAppear
+    }
+    
+    @Dependency(\.calendarClient) var calendarClient
+    
+    public init() {
+        
     }
     
     public func reduce(
@@ -16,7 +27,20 @@ public struct CalendarCore: ReducerProtocol {
     ) -> EffectTask<Action> {
         switch action {
         case .onAppear:
+            do {
+                let monthStateList = try calendarClient.createMonthStateList(-3...3, .now)
+                state.monthStateList.append(contentsOf: monthStateList)
+            } catch {
+                
+            }
             return .none
         }
     }
+}
+
+extension Date {
+    static let currentMonth: Date = {
+        let components = calendar.dateComponents([.year, .month], from: .now)
+        return calendar.date(from: components) ?? .now
+    }()
 }
