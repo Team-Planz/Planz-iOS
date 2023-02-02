29e8622cc9d4606b1c3c0c710c7746ef5e1cf60a
Andrew <escapeanaemia@gmail.com>
2023-02-02 21:13:21 +0900

    [GROW-353] 디자인 시스템 라이브러리 추가, 라이브러리 내 Color 케이스 추가 (#14)
    * [GROW-353] add DesignSystem library and following files
    
    * [GROW-354] add subcolors

---
+++ AppPackage/Sources/DesignSystem/DesignSystem.swift
@@ -0,0 +1,13 @@
+//
+//  DesignSystem.swift
+//
+//
+//  Created by 한상준 on 2023/01/28.
+//
+
+import Foundation
+
+public typealias PDS = PlanzDesignSystem
+public typealias PColor = PDS.COLOR
+
+public enum PlanzDesignSystem {}
