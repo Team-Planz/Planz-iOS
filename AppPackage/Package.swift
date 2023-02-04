b518957493f5d70c9163a8a3eab764bd12082638
GangWoon <gangwoon2018@gmail.com>
2023-02-04 12:09:12 +0900

    [GROW-356] Update TCA minimum version

--- AppPackage/Package.swift
+++ AppPackage/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.47.2"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.50.0"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.4"),
         .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0")
     ],
