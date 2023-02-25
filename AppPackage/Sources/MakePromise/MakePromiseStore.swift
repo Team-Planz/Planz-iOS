592a78bfc9f00b89a7bad3dd4cb8d9adc646122a
Andrew <escapeanaemia@gmail.com>
2023-02-25 15:21:23 +0900

    [GROW-262] 약속 생성 화면 추가  (#11)
    * update
    
    * GROW-262 Update select promise theme view
    
    * [GROW-262] update file architecture
    
    * [GROW-262] Modify AppView
    
    * [GROW-262] remove unnecessary file diff
    
    * [GROW-262] update View display logic
    
    * [GROW-262] update view
    
    * [GROW-262] add error view
    
    * [GROW-262] add error view
    
    * [GROW-262] run swift format
    
    * [GROW-262] modify to fit on swift lint rules
    
    * Automatically reformatted for Swift-lint
    
    * [GROW-262] update package
    
    * Automatically reformatted for Swift-lint
    
    * [GROW-262] delete unused files
    
    * [GROW-262] update format
    
    * [GROW-262] update access level of codes
    
    * [GROW-262] update comments
    
    * [GROW-262] remove unnecessary property
    
    * [GROW-262] update vstack alignment
    
    * [GROW-262] add text enums
    
    * [GROW-262] update color
    
    * [GROW-262] update view
    
    * [GROW-262] update texts
    
    * [GROW-262] remove print
    
    * [GROW-262] update
    
    ---------
    
    Co-authored-by: PlanzEngineering <planz.growth@gmail.com>

---
+++ AppPackage/Sources/MakePromise/MakePromiseStore.swift
@@ -0,0 +1,51 @@
+//
+//  MakePromiseStore.swift
+//  Planz
+//
+//  Created by 한상준 on 2022/12/14.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import ComposableArchitecture
+import Foundation
+
+public struct MakePromiseEnvironment {
+    public init() {}
+}
+
+public let makePromiseReducer = Reducer<MakePromiseState, MakePromiseAction, MakePromiseEnvironment>.combine(
+    makePromiseSelectThemeReducer
+        .optional()
+        .pullback(
+            state: \.selectTheme,
+            action: /MakePromiseAction.selectTheme,
+            environment: { _ in SelectThemeEnvironment() }
+        ),
+    makePromiseSetNameAndPlaceReducer
+        .optional()
+        .pullback(
+            state: \.setNameAndPlace,
+            action: /MakePromiseAction.setNameAndPlace,
+            environment: { _ in SetNameAndPlaceEnvironment() }
+        ),
+    Reducer { state, action, _ in
+        switch action {
+        case .nextButtonTapped:
+            if state.isNextButtonEnable() {
+                state.moveNextStep()
+                state.updateBackButtonVisibleState()
+            }
+            return .none
+        case .backButtonTapped:
+            state.movePastStep()
+            state.updateBackButtonVisibleState()
+            return .none
+        case let .selectTheme(.promiseTypeListItemTapped(promiseType)):
+            return .none
+        case let .setNameAndPlace(.filledPromiseName(name)):
+            return .none
+        case let .setNameAndPlace(.filledPromisePlace(place)):
+            return .none
+        }
+    }
+)
---
+++ AppPackage/Sources/MakePromise/SubViews/TopInformationView.swift
@@ -0,0 +1,49 @@
+//
+//  TopInformationView.swift
+//  Planz
+//
+//  Created by 한상준 on 2022/10/08.
+//  Copyright © 2022 Team-Planz. All rights reserved.
+//
+
+import ComposableArchitecture
+import DesignSystem
+import SwiftUI
+
+public struct TopInformationView: View {
+    var store: Store<MakePromiseState, MakePromiseAction>
+    public var body: some View {
+        WithViewStore(self.store) { viewStore in
+            VStack(spacing: 4) {
+                HStack {
+                    Spacer().frame(width: 20)
+                    HStack {
+                        Text("\(viewStore.index + 1)")
+                            .foregroundColor(PDS.COLOR.gray8.scale)
+                        Text("/ \(viewStore.steps.count)").foregroundColor(PDS.COLOR.gray3.scale)
+                    }
+
+                    Spacer()
+                    Button(action: {
+                        // TODO: (Andrew) 상위 뷰에서 해당 뷰를 닫아주는 로직 추가 예정
+                    }) {
+                        Image(systemName: PlanzText.xmark.rawValue)
+                    }
+                    Spacer().frame(width: 20)
+                }
+                HStack {
+                    Spacer().frame(width: 20)
+                    Text(PlanzText.themeSelectTitle.rawValue)
+                        .bold()
+                    Spacer()
+                }
+            }
+        }
+    }
+
+    // TODO: (Andrew) 모듈화 필요
+    enum PlanzText: String {
+        case xmark
+        case themeSelectTitle = "약속 테마를 선택해 주세요!"
+    }
+}
