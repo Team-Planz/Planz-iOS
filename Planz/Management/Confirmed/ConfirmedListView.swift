d4640d7c533caa57f9a1193eb742e481a2064c4a
Andrew <escapeanaemia@gmail.com>
2023-03-05 16:49:25 +0900

    [GROW-280] update format

--- Planz/Management/Confirmed/ConfirmedListView.swift
+++ Planz/Management/Confirmed/ConfirmedListView.swift
@@ -11,16 +11,16 @@ import SwiftUI
 struct ConfirmedListFeature: ReducerProtocol {
     struct State: Equatable {
         var rows: IdentifiedArrayOf<ConfirmedCell.State>
-        
+
         init(rows: IdentifiedArrayOf<ConfirmedCell.State> = []) {
             self.rows = rows
         }
     }
-    
+
     enum Action: Equatable {
         case pushDetailView(id: ConfirmedCell.State.ID, action: ConfirmedCell.Action)
     }
-    
+
     var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
@@ -38,10 +38,10 @@ struct ConfirmedListFeature: ReducerProtocol {
 }
 
 // MARK: - PromiseListView
+
 struct ConfirmedListView: View {
-    
     let store: StoreOf<ConfirmedListFeature>
-    
+
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             Group {
@@ -51,9 +51,10 @@ struct ConfirmedListView: View {
                     List {
                         ForEachStore(self.store.scope(
                             state: \.rows,
-                            action: ConfirmedListFeature.Action.pushDetailView(id: action:))) {
-                                ConfirmedCellView(store: $0)
-                            }
+                            action: ConfirmedListFeature.Action.pushDetailView(id: action:)
+                        )) {
+                            ConfirmedCellView(store: $0)
+                        }
                     }
                     .listStyle(.plain)
                 }
@@ -63,24 +64,25 @@ struct ConfirmedListView: View {
 }
 
 extension IdentifiedArray where ID == ConfirmedCell.State.ID, Element == ConfirmedCell.State {
-  static let mock: Self = [
-    ConfirmedCell.State(
-        id: UUID(), title: "확정 약속1", role: .general, leaderName: "김세현", replyPeopleCount: 3
-    ),
-    ConfirmedCell.State(
-        id: UUID(), title: "확정 약속2", role: .leader, leaderName: "강빛나", replyPeopleCount: 5
-    ),
-    ConfirmedCell.State(
-        id: UUID(), title: "확정 약속3", role: .general, leaderName: "한지희", replyPeopleCount: 8
-    )
-  ]
+    static let mock: Self = [
+        ConfirmedCell.State(
+            id: UUID(), title: "확정 약속1", role: .general, leaderName: "김세현", replyPeopleCount: 3
+        ),
+        ConfirmedCell.State(
+            id: UUID(), title: "확정 약속2", role: .leader, leaderName: "강빛나", replyPeopleCount: 5
+        ),
+        ConfirmedCell.State(
+            id: UUID(), title: "확정 약속3", role: .general, leaderName: "한지희", replyPeopleCount: 8
+        )
+    ]
 }
 
 struct ConfirmedListView_Previews: PreviewProvider {
     static var previews: some View {
         ConfirmedListView(store: StoreOf<ConfirmedListFeature>(
             initialState: ConfirmedListFeature.State(rows: .mock),
-            reducer: ConfirmedListFeature())
+            reducer: ConfirmedListFeature()
+        )
         )
     }
 }
--- Planz/PlanzApp.swift
+++ Planz/PlanzApp.swift
@@ -6,19 +6,13 @@
 //  Copyright © 2022 Team-Planz. All rights reserved.
 //
 
-import MakePromise
 import SwiftUI
+
 @main
 struct PlanzApp: App {
     var body: some Scene {
         WindowGroup {
-            MakePromiseView(
-                store: .init(
-                    initialState: .init(),
-                    reducer: makePromiseReducer,
-                    environment: .init()
-                )
-            )
+            AppView()
         }
     }
 }
