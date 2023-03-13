deb48cfe54974d068de983ff8460f30826fb4108
Jinsujin <sylpid003@gmail.com>
2023-03-13 16:41:34 +0900

    [PI-3] 약속 상세화면 frame size 을 비율값으로 조정

--- AppPackage/Sources/PromiseManagement/ConfirmedDetail/ConfirmedDetailView.swift
+++ AppPackage/Sources/PromiseManagement/ConfirmedDetail/ConfirmedDetailView.swift
@@ -51,66 +51,70 @@ public struct ConfirmedDetailView: View {
 
     public var body: some View {
         WithViewStore(self.store) { viewStore in
-            NavigationView {
-                VStack {
-                    Image(illustImageName, bundle: Bundle.module)
-                        .zIndex(1)
-                        .offset(y: 32)
+            GeometryReader { proxy in
+                NavigationView {
+                    VStack {
+                        Image(illustImageName, bundle: Bundle.module)
+                            .resizable()
+                            .zIndex(1)
+                            .offset(y: 32)
+                            .frame(width: 64, height: 64)
 
-                    VStack(alignment: .leading) {
-                        Group {
-                            Text(viewStore.theme)
-                                .bold()
-                                .font(.title)
-                                .foregroundColor(PColor.purple9.scale)
+                        VStack(alignment: .leading) {
+                            Group {
+                                Text(viewStore.theme)
+                                    .bold()
+                                    .font(.title)
+                                    .foregroundColor(PColor.purple9.scale)
 
-                            Text(viewStore.title)
-                                .foregroundColor(PColor.cGray2.scale)
+                                Text(viewStore.title)
+                                    .foregroundColor(PColor.cGray2.scale)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(PColor.gray3.scale)
+
+                            VStack(alignment: .leading, spacing: 16) {
+                                makeContentView(
+                                    title: "날짜/시간",
+                                    content: viewStore.date
+                                )
+
+                                makeContentView(
+                                    title: "장소",
+                                    content: viewStore.place
+                                )
+
+                                makeContentView(
+                                    title: "참여자",
+                                    content: viewStore.participants.joined(separator: ", ")
+                                )
+                            }
+                            .padding(.top)
                         }
-                        .frame(maxWidth: .infinity, alignment: .center)
-
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(PColor.gray3.scale)
-
-                        VStack(alignment: .leading, spacing: 16) {
-                            makeContentView(
-                                title: "날짜/시간",
-                                content: viewStore.date
-                            )
-
-                            makeContentView(
-                                title: "장소",
-                                content: viewStore.place
-                            )
-
-                            makeContentView(
-                                title: "참여자",
-                                content: viewStore.participants.joined(separator: ", ")
-                            )
-                        }
-                        .padding(.top)
-                    }
-                    .padding(EdgeInsets(top: 32, leading: 20, bottom: 32, trailing: 20))
-                    .background(PColor.gray1.scale)
-                    .frame(width: 340, alignment: .leading)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(PColor.gray3.scale, lineWidth: 1)
-                    )
-                    .navigationTitle("약속 상세보기")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        ToolbarItem {
-                            Button {
-                                viewStore.send(.dismissed)
-                            } label: {
-                                Image(closeImageName, bundle: Bundle.module)
+                        .padding(EdgeInsets(top: 32, leading: 20, bottom: 32, trailing: 20))
+                        .background(PColor.gray1.scale)
+                        .frame(width: proxy.size.width * 0.9, alignment: .leading)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(PColor.gray3.scale, lineWidth: 1)
+                        )
+                        .navigationTitle("약속 상세보기")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            ToolbarItem {
+                                Button {
+                                    viewStore.send(.dismissed)
+                                } label: {
+                                    Image(closeImageName, bundle: Bundle.module)
+                                }
                             }
                         }
                     }
+                    .offset(y: -proxy.size.height * 0.1)
                 }
-                .offset(y: -80)
             }
         }
     }
@@ -125,3 +129,29 @@ public struct ConfirmedDetailView: View {
         }
     }
 }
+
+#if DEBUG
+    struct ConfirmedDetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            ConfirmedDetailView(store:
+                .init(
+                    initialState:
+                    ConfirmedDetailFeature.State(
+                        ConfirmedCell.State(
+                            id: UUID(),
+                            title: "약속명",
+                            role: .leader,
+                            leaderName: "파티장이름",
+                            replyPeopleCount: 6,
+                            theme: "여행",
+                            date: "2023",
+                            place: "강남",
+                            participants: ["정인혜", "이은영"]
+                        )
+                    ),
+                    reducer: ConfirmedDetailFeature()._printChanges()
+                )
+            )
+        }
+    }
+#endif
