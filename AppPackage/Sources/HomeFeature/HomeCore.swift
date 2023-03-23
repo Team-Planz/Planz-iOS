1f1385b64492771e8e4c7a6fede31b1ce0ecd836
GangWoon <gangwoon2018@gmail.com>
2023-03-23 22:41:53 +0900

    [PI-11] Add home core

---
+++ AppPackage/Sources/HomeFeature/HomeCore.swift
@@ -0,0 +1,46 @@
+import CalendarFeature
+import ComposableArchitecture
+import Foundation
+
+public struct HomeCore: ReducerProtocol {
+    public struct State: Equatable {
+        var todayPromiseList: [Promise] {
+            calendar.monthList[id: .currentMonth]?.monthState[.today] ?? []
+        }
+
+        public var calendar: CalendarCore.State
+
+        public init(calendar: CalendarCore.State = .init()) {
+            self.calendar = calendar
+        }
+    }
+
+    public enum Action: Equatable {
+        case onAppear
+        case rowTapped(Date)
+        case calendar(action: CalendarCore.Action)
+    }
+
+    public init() {}
+
+    public var body: some ReducerProtocol<State, Action> {
+        Scope(
+            state: \.calendar,
+            action: /Action.calendar,
+            child: CalendarCore.init
+        )
+
+        Reduce { _, action in
+            switch action {
+            case .onAppear:
+                return .none
+
+            case .rowTapped:
+                return .none
+
+            case .calendar:
+                return .none
+            }
+        }
+    }
+}
