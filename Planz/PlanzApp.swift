43812a8f7fbdb7fabcf9e0ab3960d8517f02168a
Andrew <escapeanaemia@gmail.com>
2023-04-04 19:18:49 +0900

    [PI-36] add app delegate

--- Planz/PlanzApp.swift
+++ Planz/PlanzApp.swift
@@ -1,9 +1,23 @@
 import AppFeature
 import ComposableArchitecture
+import Firebase
+import ShareFeature
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct PlanzApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     let store: StoreOf<AppCore> = .init(
         initialState: .launchScreen,
         reducer: AppCore()
