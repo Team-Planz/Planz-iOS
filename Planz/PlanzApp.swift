0c747fc2ca6522e7b048cc1a1f89b8655d6c5ece
Andrew <escapeanaemia@gmail.com>
2023-04-07 23:11:26 +0900

    [PI-35] rollback PlanzApp changes

--- Planz/PlanzApp.swift
+++ Planz/PlanzApp.swift
@@ -1,20 +1,17 @@
 import AppFeature
 import ComposableArchitecture
-import PromiseTimeDetailFeature
 import SwiftUI
-import TimeTableFeature
+
 @main
 struct PlanzApp: App {
     let store: StoreOf<AppCore> = .init(
         initialState: .launchScreen,
         reducer: AppCore()
     )
-    let store2: StoreOf<PromiseTimeDetailFeature> = .init(initialState: .init(), reducer: PromiseTimeDetailFeature())
 
     var body: some Scene {
         WindowGroup {
-//            AppView(store: store)
-            PromiseTimeDetailView(store: store2)
+            AppView(store: store)
         }
     }
 }
